9d742fe6b2b2adb465f610a1bd6445c7ac2e1e33
mihirjoshi07 <60389676+mihirjoshi07@users.noreply.github.com>
2024-11-07 13:32:31 +0530

    Update ResetPassword.jsx

--- src/components/pages/ResetPassword.jsx
+++ src/components/pages/ResetPassword.jsx
@@ -162,7 +162,7 @@ export default function OTPInput() {
 
     const handleSubmit = async () => {
         try {
-            const response = await fetch('http://13.126.11.73:3000/admin/validateOtp', {
+            const response = await fetch('http://3.110.108.114:3000/admin/validateOtp', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
