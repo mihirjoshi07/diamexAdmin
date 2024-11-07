d6fe783246c18741738a64c468a3e665aad54841
mihirjoshi07 <60389676+mihirjoshi07@users.noreply.github.com>
2024-11-07 16:19:52 +0530

    Update PasswordChange.jsx

--- src/components/pages/PasswordChange.jsx
+++ src/components/pages/PasswordChange.jsx
@@ -96,7 +96,7 @@ const PasswordChange = () => {
         setErrors({}); // Clear any existing errors
 
         try {
-            const response = await fetch("http://3.110.108.114:3000/admin/change-password", {
+            const response = await fetch("http://13.235.134.187:3000/admin/change-password", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
